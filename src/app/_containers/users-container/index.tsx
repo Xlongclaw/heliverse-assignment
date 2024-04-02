970c254e5fd556bda62adbdc32bc7185bace2c7d
unknown <xlongclaw@gmail.com>
2024-04-02 22:44:47 +0530

    feat: server Connection

--- src/app/_containers/users-container/index.tsx
+++ src/app/_containers/users-container/index.tsx
@@ -5,18 +5,24 @@ import { Pagination } from "@nextui-org/react";
 import { LayoutGroup, motion } from "framer-motion";
 
 export default function UsersContainer() {
-  const [currentPage, setCurrentPage] = React.useState(0);
-  const usersData = React.useContext(UsersContext);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const { users, changePage, pageCount } = React.useContext(UsersContext);
+
+  React.useEffect(() => {
+    console.log(currentPage);
+    changePage(currentPage);
+  }, [currentPage]);
+
   return (
     <section className="px-[9rem] ">
       <motion.div className="grid grid-cols-4 gap-2">
-        {usersData.users.map((user) => (
+        {users.map((user) => (
           <UserDisplayCard user={user} key={`USER_${user.id}`} />
         ))}
       </motion.div>
       <div className="flex py-8 items-center justify-center w-full">
         <Pagination
-          total={10}
+          total={pageCount}
           color="primary"
           page={currentPage}
           onChange={setCurrentPage}
--- src/providers/users-provider/index.tsx
+++ src/providers/users-provider/index.tsx
@@ -1,4 +1,4 @@
-import { usersData } from "@/constants";
+// import { usersData } from "@/constants";
 import { UserType } from "@/types";
 import React from "react";
 
@@ -8,13 +8,40 @@ interface IProps {
 
 interface IUsersContext {
   users: Array<UserType>;
+  changePage: (page: number) => void;
+  pageCount: number;
 }
 
-export const UsersContext = React.createContext<IUsersContext>({ users: [] });
+const fetchUsers = async (page: number) => {
+  const res = await fetch(`http://localhost:8080/api/users?page=${page}`);
+  const data = await res.json();
+  return { users: data.users, count: data.count };
+};
+
+export const UsersContext = React.createContext<IUsersContext>({
+  users: [],
+  changePage: () => {},
+  pageCount: 0,
+});
 
 export default function UsersProvider({ children }: IProps) {
+  const [usersData, setUsersData] = React.useState<Array<UserType>>([]);
+  const [page, setPage] = React.useState(1);
+  const [pageCount, setPageCount] = React.useState(1);
+
+  const changePage = (page: number) => {
+    setPage(page);
+  };
+
+  React.useEffect(() => {
+    fetchUsers(page).then((data) => {
+      setPageCount(data.count / 20);
+      setUsersData(data.users);
+    });
+  }, [page]);
+
   return (
-    <UsersContext.Provider value={{ users: usersData }}>
+    <UsersContext.Provider value={{ users: usersData, changePage, pageCount }}>
       {children}
     </UsersContext.Provider>
   );
