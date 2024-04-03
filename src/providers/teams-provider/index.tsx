c489513d5e913467fcbe2c66a78e7c2cf9dcb61f
unknown <xlongclaw@gmail.com>
2024-04-03 20:48:52 +0530

    feat(state): add new teams-provider

---
+++ src/providers/teams-provider/index.tsx
@@ -0,0 +1,50 @@
+import { SERVER_URL } from "@/constants";
+import React from "react";
+
+interface IProps {
+  children: React.JSX.Element;
+}
+
+interface ITeamsContext {
+  teams: Array<ITeam>;
+  postTeam: (data: Array<{ teamName: string; members: Array<number> }>) => void;
+}
+
+interface ITeam {
+  teamName: string;
+  members: number[];
+}
+
+const fetchAllTeams = async () => {
+  const res = await fetch(`${SERVER_URL}/api/team`);
+  const data = await res.json();
+  return data.teams;
+};
+
+export const TeamsContext = React.createContext<ITeamsContext | null>(null);
+
+export default function TeamsProvider({ children }: IProps) {
+  const [teams, setTeams] = React.useState<Array<ITeam>>();
+
+  React.useEffect(() => {
+    fetchAllTeams().then((data) => setTeams(data));
+  }, []);
+
+  const postTeam = (
+    data: Array<{ teamName: string; members: Array<number> }>
+  ) => {
+    fetch(`${SERVER_URL}/api/team`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
+
+  return (
+    <TeamsContext.Provider value={{ teams: teams!, postTeam }}>
+      {teams && children}
+    </TeamsContext.Provider>
+  );
+}
